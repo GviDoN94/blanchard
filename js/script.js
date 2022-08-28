9b42c3ac9eac5feff1f1843d1920e93c7dfa8101
Vladislav Surin <vlad_surin@mail.ru>
2022-08-28 21:27:09 +0300

    added js for dropdown menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,39 @@
 "use strict";
+
+// /* Dropdown menu */
+const dropMenu = document.querySelector(".dropdown-menu");
+const dropMenuBtns = document.querySelectorAll(".dropdown-menu__btn");
+const drops = document.querySelectorAll(".dropdown");
+
+dropMenu.addEventListener("click", (event) => {
+  const activeDrop = event.target;
+  if (!activeDrop.classList.contains("dropdown-menu__btn")) {
+    return;
+  }
+
+  dropMenuBtns.forEach((btn) => {
+    if (activeDrop == btn) {
+      const drop = btn
+        .closest(".dropdown-menu__item")
+        .querySelector(".dropdown");
+      drop.classList.toggle("dropdown--active");
+      drops.forEach((elem) => {
+        if (elem != drop) {
+          elem.classList.remove("dropdown--active");
+        }
+      });
+      btn.classList.toggle("dropdown-menu__btn--active");
+    } else {
+      btn.classList.remove("dropdown-menu__btn--active");
+    }
+  });
+
+  document.addEventListener("click", (el) => {
+    if (!el.target.closest(".dropdown-menu__item")) {
+      dropMenuBtns.forEach((btn) =>
+        btn.classList.remove("dropdown-menu__btn--active")
+      );
+      drops.forEach((drop) => drop.classList.remove("dropdown--active"));
+    }
+  });
+});
